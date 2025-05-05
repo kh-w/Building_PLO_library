4493dac3cf1752b486a964ccb40c10625142f473
kh-w <kinhang@gmail.com>
2025-05-05 09:41:07 -0500

    a list of functions to build

---
+++ Functions_to_build.R
@@ -0,0 +1,22 @@
+# identify characteristics of a PLO starting hand (4 hole cards)
+
+# Rundown
+# Double suited
+# Single suited
+# Pairs
+# Double pairs
+# 9 or above
+# 8 or below
+
+
+
+# identify hand strength and improvability at flop, turn, river
+
+# Made Hand check
+# Drawing possibility and their probability
+
+
+
+# Identify board texture
+
+# Possible draws of opponents (considering player's blockers)
