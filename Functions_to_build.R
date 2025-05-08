a4a113f89c7238fa8d9d2c5ed6b3e93b48bb15c1
kh-w <kinhang@gmail.com>
2025-05-07 20:45:35 -0500

    More preparation

--- Functions_to_build.R
+++ Functions_to_build.R
@@ -1,21 +1,29 @@
-# identify characteristics of a PLO starting hand (4 hole cards)
+### Reading our own hands
 
-# Rundown
+## identify characteristics of a PLO starting hand (4 hole cards)
+
+# Connectivity check (3/4-cards, High/Medium/Low-rundowns, (m,n,k)-gap/(m,n)-gap)
 # Double suited
 # Single suited
 # Pairs
 # Double pairs
 # 9 or above
 # 8 or below
+# Rainbow check
 
-
-
-# identify hand strength and improvability at flop, turn, river
+## identify hand strength and improvability at flop, turn, river
 
 # Made Hand check
 # Drawing possibility and their probability
+# Backdoor Drawing possibility and their probability
 
 
+### Functions to get opponents range by user input
+
+# Range(f_pct, patterns). User input: frequency and patterns, i.e. f% and c("double suited", "9 or above")
+
+
+### Reading the situation as a whole
 
 # Identify board texture
 
